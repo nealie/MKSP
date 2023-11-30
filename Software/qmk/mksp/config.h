f33767cfe2ef355e7c175b01673880905dd376b6
Neal Nelson <neal@nicandneal.net>
2023-11-30 15:44:32 +0100

    Add QMK setup.

---
+++ Software/qmk/mksp/config.h
@@ -0,0 +1,20 @@
+// Copyright 2023 Neal Nelson (@Neal Nelson)
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
